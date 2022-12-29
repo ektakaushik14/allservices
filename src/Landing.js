b6756411d718f4fa5b6240848aaab962de6d48a1
Ektakaushik <105712212+Ektakaushik@users.noreply.github.com>
2022-12-29 11:43:28 +0530

    Landing-design

---
+++ src/Landing.js
@@ -0,0 +1,20 @@
+import "./Allcss.css";
+export default function Landing() {
+  return (
+    <div className="landing">
+      <div className="landing-page">
+        <div className="land-section2"></div>
+        <div className="land-section3"></div>
+        <div className="land-section1"></div>
+        <div className="landing-text">
+          <div className="land-title">E-wallet loaded with features!</div>
+          <div className="land-button">
+            <button>
+              <span> Get Started</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
