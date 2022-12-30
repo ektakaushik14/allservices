5bee8cff3d6c75fd8e931e095a88e7a79323cdc5
Ektakaushik <105712212+Ektakaushik@users.noreply.github.com>
2022-12-30 07:27:45 +0530

    Service-design

---
+++ src/Components/Services/servicesConstants.js
@@ -0,0 +1,53 @@
+import TipsAndUpdatesSharpIcon from "@mui/icons-material/TipsAndUpdatesSharp";
+const services = [
+  {
+    heading: "Write copy & content that converts",
+    subheading:
+      "Your AI sidekick is trained to produce high-converting marketing copy",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Brainstorm new ideas.",
+    subheading:
+      "Breakthrough writer’s block with Jasper who will help you spark fresh ideas.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "10X your content output",
+    subheading:
+      "Whether you're a creator or director of a content team, Jasper helps you scale up.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Write better, everywhere.",
+    subheading:
+      "Whether you’re writing emails, captions, or love letters, Jasper will help craft the perfect message.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Create content in 26 languages",
+    subheading:
+      "Jasper can read and write content in 25 languages including English, Spanish, Japanese, and Portuguese.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Build an AI-Powered business.",
+    subheading:
+      "Empower your team or customers with an AI sidekick to improve your processes and product.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Collaborate with your team",
+    subheading:
+      "Invite team members from all around the world to work on content and see updates in real-time.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+  {
+    heading: "Have fun! Jasper is a joy.",
+    subheading:
+      "AI is an exciting, new technology that can unlock your imagination to create some amazing things.",
+    icon: <TipsAndUpdatesSharpIcon />,
+  },
+];
+
+export default services;
