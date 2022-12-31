3862ef87b7d24a5a0dbf24d64fe25e6f5932b0c4
pushkarxoxo <pushkar@xoxoday.com>
2022-12-31 23:47:57 +0530

    support page done

--- src/Components/Support/Support.js
+++ src/Components/Support/Support.js
@@ -11,33 +11,36 @@ export default function Support() {
         <div>For general inquiries: shanumishra@gmail.com </div>
         <div>Call us anytime at: +9999999999 </div>
       </div>
-      <div>
+      <div className="supportSubHeading">
         <form>
           <div>
             <div>
               <div>
                 <h4>first name</h4>
-                <input type="text" />
+                <input placeholder="e.g. Jim" type="text" />
               </div>
               <div>
                 <h4>last name</h4>
-                <input type="text" />
+                <input placeholder="e.g. Potter" type="text" />
               </div>
             </div>
             <div>
               <div>
                 <h4>Email</h4>
-                <input type="text" />
+                <input placeholder="e.g. example@gmail.com" type="text" />
               </div>
               <div>
                 <h4>Phone Number</h4>
-                <input type="text" />
+                <input placeholder="e.g. 123-4567-89" type="text" />
               </div>
             </div>
           </div>
-          <div>
+          <div className="supportMessage">
             <h4>Message</h4>
             <input type="text" />
+          </div>
+          <div className="supportSubmitButton">
+            <button>Submit</button>
           </div>
         </form>
       </div>
