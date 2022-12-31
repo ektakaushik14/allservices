8ea753f2ebf4be05f83bc1fe170d68d8ec57d060
pushkarxoxo <pushkar@xoxoday.com>
2022-12-31 20:15:55 +0530

    login and signup done

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,9 +1,22 @@
 import "./landing.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 import Nav from "../Nav/Nav";
+import { useState } from "react";
 export default function Landing() {
+  const [loggedOut, setLoggedOut] = useState(false);
+  const isSignedOut = (e) => {
+    console.log(loggedOut);
+    setLoggedOut(e);
+    setTimeout(() => {
+      setLoggedOut(false);
+      console.log(loggedOut);
+    }, 5000);
+  };
+
   return (
     <div className="landing">
-      <Nav />
+      <Nav isSignedOut={(e) => isSignedOut(e)} />
       <div className="landing-page">
         <div className="land-section2"></div>
         <div className="land-section3"></div>
@@ -17,6 +30,7 @@ export default function Landing() {
           </div>
         </div>
       </div>
+      {loggedOut && <div className="signInPrompt">Signed out successfully</div>}
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./login.css";
+import Loader from "../../Images/loader.png";
+import { auth } from "../../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 export default function Login() {
+  const navigate = useNavigate();
+
+  const [loginSignUp, setLoginSignUp] = useState(1);
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    setLoading(true);
+    if (loginSignUp === 1) {
+      signInWithEmailAndPassword(auth, username, password)
+        .then((creds) => {
+          console.log(creds);
+          setLoading(false);
+          setError("");
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+          const errorMessage = err.message;
+          setError(errorMessage);
+          setLoading(false);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, username, password)
+        .then((creds) => {
+          console.log(creds);
+          setLoading(false);
+          setError("");
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+          const errorMessage = err.message;
+          setError(errorMessage);
+          setLoading(false);
+        });
+    }
+  };
+
+  const provider = new GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        navigate("/");
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  useEffect(() => {
+    setError("");
+  }, [loginSignUp, name, username, password]);
+
   return (
     <div className="login-page">
       <div className="login-left-side">
@@ -22,31 +102,85 @@ export default function Login() {
             <div>Lorem ipsum dolor sit amet.</div>
           </div>
           <div className="login-form">
+            {loginSignUp != 1 && (
+              <div>
+                <h4>Name</h4>
+                <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                />
+              </div>
+            )}
             <div>
               <h4>email</h4>
-              <input type="email" />
+              <input
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                type="email"
+              />
             </div>
             <div>
               <h4>password</h4>
-              <input type="password" />
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+              />
             </div>
           </div>
+          {error.length > 0 && (
+            <div className="error-message">
+              {error.includes("email-already-in-use")
+                ? "Email is already in use please Login"
+                : error}
+            </div>
+          )}
           <div className="forgot-password">
             <div>
               <input type="checkbox" /> remember me
             </div>
             <div>Forgot Password?</div>
           </div>
-          <div className="login-button">
-            <button>Login</button>
+          <div onClick={handleLogin} className="login-button">
+            <button>
+              {loading ? (
+                <img src={Loader} />
+              ) : loginSignUp === 1 ? (
+                "Login"
+              ) : (
+                "Sign Up"
+              )}{" "}
+            </button>
           </div>
+          {loginSignUp === 1 ? (
+            <div>
+              Don't have an account?{" "}
+              <span
+                style={{ color: "blue", cursor: "pointer" }}
+                onClick={() => setLoginSignUp(2)}
+              >
+                Sign Up
+              </span>
+            </div>
+          ) : (
+            <div>
+              Already have an account?{" "}
+              <span
+                style={{ color: "blue", cursor: "pointer" }}
+                onClick={() => setLoginSignUp(1)}
+              >
+                Login
+              </span>
+            </div>
+          )}
           <div className="login-divider">
             <div></div>
             <div>or</div>
             <div></div>
           </div>
           <div className="googleLogin">
-            <button>
+            <button onClick={signInWithGoogle}>
               {" "}
               <img
                 src="https://img.icons8.com/fluency/512/google-logo.png"
