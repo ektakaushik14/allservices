6f55087d86e53dc136fd3bcfac04f525cb9588e7
Ektakaushik <105712212+Ektakaushik@users.noreply.github.com>
2022-12-31 22:36:26 +0530

    Landing-color-update

---
+++ src/Components/Pricing/Pricing.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Pricing() {
+  return (
+    <div className="pricing">
+      <div className="price-title">
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus,
+          velit.
+        </div>
+        <div>
+          <img
+            src="https://ouch-cdn2.icons8.com/3Qf5abNYby5z4TNL3RLXrl47R3xoOVKZ4HBQZi7Oog8/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMjUx/LzA5ZTI2MGQ2LTEy/NWMtNGE2Ny1iMmE0/LTJlMTJhM2MwNmFh/ZS5zdmc.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="price-card"></div>
+    </div>
+  );
+}
