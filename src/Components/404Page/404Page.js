1f080fec1cfb484faefb0f033ff2caf0007843c7
pushkarxoxo <pushkar@xoxoday.com>
2023-01-01 18:55:03 +0530

    added 404 page

--- src/Components/404Page/404Page.js
+++ src/Components/404Page/404Page.js
@@ -1,5 +1,22 @@
 import React from "react";
-
+import "./pageNotFound.css";
+import Hand from "../../Images/3dHand.png";
+import { Link } from "react-router-dom";
 export default function PageNotFound() {
-  return <div>Page not Found</div>;
+  return (
+    <div className="pageNotFound">
+      <div className="pageNotFoundContent">
+        we couldn't <br /> find the <br /> page you <br /> were <br /> looking
+        for
+      </div>
+      <div className="pageNotFoundButton">
+        <button>
+          <Link to="/">back home</Link>
+        </button>
+      </div>
+      <div className="pageNotFoundImage">
+        <img src={Hand} alt="" />
+      </div>
+    </div>
+  );
 }
