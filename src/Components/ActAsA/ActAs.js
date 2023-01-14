ebdcc170ab8dcdb5f9d245584d97c903cd131234
pushkarxoxo <pushkar@xoxoday.com>
2023-01-14 23:00:36 +0530

    made changes

--- src/Components/ActAsA/ActAs.js
+++ src/Components/ActAsA/ActAs.js
@@ -6,6 +6,9 @@ import { data, getImage } from "./IndividualAct";
 import CompanyLogo from "../../Images/CompanyLogo.png";
 import UserImg from "../../Images/userImg.png";
 import WelcomePageImg from "../../Images/welcomePage.png";
+import Sun from "../../Images/sun.png";
+import Moon from "../../Images/moon.png";
+import Send from "../../Images/send.png";
 
 export default function ActAs() {
   const [openChat, setOpenChat] = useState();
@@ -14,16 +17,28 @@ export default function ActAs() {
   const [conversation, setConversation] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [chatArray, setChatArray] = useState([
+    { questions: [] },
+    { answer: [] },
+  ]);
+
   const handleSelectUser = (random, name, key) => {
     setOpenChat((prev) => (prev = { name: name, img: random, index: key }));
   };
 
   const handleChatContent = (content, chatName) => {
     setIsLoading(true);
+
     setChatContent({
       ...chatContent,
       [chatName]: { ask: content },
     });
+
+    const updatedChatArray = [...chatArray];
+
+    console.log(updatedChatArray);
+    // updatedChatArray[0].questions.push(content);
+    // setChatArray(updatedChatArray);
   };
 
   useEffect(() => {
@@ -37,6 +52,9 @@ export default function ActAs() {
 
       if (chatData && chatData["ask"]) {
         data(chatData["ask"], openChat.name).then((data) => {
+          let updatedChatArray = [...chatArray];
+          // updatedChatArray[1].answer.push(data);
+          // setChatArray(updatedChatArray);
           setIsLoading(false);
           setConversation((prev) => [
             ...prev,
@@ -47,12 +65,18 @@ export default function ActAs() {
     }
   }, [chatContent]);
 
+  const handlePushAnswer = async () => {};
+
   useEffect(() => {
     var openChatElement = document.querySelector(".openChatSection");
     if (openChatElement) {
       openChatElement.scrollTop = openChatElement.scrollHeight;
     }
   }, [conversation]);
+
+  useEffect(() => {
+    console.log(chatArray, ">>>>>");
+  }, [chatArray]);
 
   const Chat = ({ name, index }) => {
     const random = Math.floor(Math.random() * constants.length) + 1;
@@ -88,17 +112,16 @@ export default function ActAs() {
             alt=""
           />
           <div className="openChatHeaderNameContainer">
-            <div className="openChatHeaderName">{openChat.name}</div>
-            <div className="openChatHeaderTime">last active now</div>
+            <div className="openChatHeaderName">
+              Conversation with <b>{openChat.name}</b>
+            </div>
           </div>
           <div className="openChatHeaderOptions">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>Clear Chat</div>
           </div>
         </div>
         <div className="openChatSection">
-          {conversation.map((name) => {
+          {/* {conversation.map((name) => {
             if (Object.keys(name)[0] === openChat.name) {
               return (
                 <>
@@ -122,18 +145,21 @@ export default function ActAs() {
               );
             }
           })}
-          {isLoading ? <div>Loading...</div> : null}
+          {isLoading ? <div>Loading...</div> : null} */}
         </div>
-        <div className="openChatInput">
-          <input
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleChatContent(e.target.value, openChat.name);
-              }
-            }}
-            type="text"
-            placeholder="Ask anything..."
-          />
+        <div className="openChatInputContainer">
+          <div className="openChatInput">
+            <input
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleChatContent(e.target.value, openChat.name);
+                }
+              }}
+              type="text"
+              placeholder="Ask anything..."
+            />
+            <img src={Send} alt="" />
+          </div>
         </div>
       </div>
     );
@@ -149,35 +175,92 @@ export default function ActAs() {
   };
 
   return (
-    <div className="actAs">
-      <div className="activeAsAChats">
-        <div>
-          <div>Inbox</div>
-          <div className="individualChatSearch">
-            <input
-              value={inputSearch}
-              onChange={(e) => {
-                setInputSearch(e.target.value);
-              }}
-              type="text"
-            />
-            <img src={Search} alt="" />
+    <div className="actAsContainer">
+      <div className="actAsHeaderContainer">
+        <div className="actAsHeader">
+          <div className="actAsHeaderLogo">
+            <img src={CompanyLogo} alt="" />
+          </div>
+          <div className="actAsHeaderContent">
+            <div className="themeSwitcher">
+              {/* <div>
+                <input type="checkbox" class="checkbox" id="checkbox" />
+                <label for="checkbox" class="label">
+                  <img src={Moon} class="fas fa-moon" />
+                  <img src={Sun} class="fas fa-sun" />
+                  <div class="ball" />
+                </label>
+              </div> */}
+            </div>
+            <div>Good Morning, John</div>
+            <div>S</div>
           </div>
         </div>
-        <div>
-          <div className="activeAsAChatsActive">Unlocked</div>
-          <div>Locked</div>
-        </div>
-        <div className="individualChatContainer">
-          {constants
-            .filter((name) => name.toLowerCase().includes(inputSearch))
-            .map((name, key) => (
-              <Chat key={key} index={key} name={name} />
-            ))}
-        </div>
       </div>
-      {openChat == null ? <WelcomePage /> : <OpenChat />}
-      <div></div>
+      <div className="actAs">
+        <div className="activeAsAChats">
+          <div>
+            <div className="individualChatSearch">
+              <input
+                value={inputSearch}
+                placeholder="Search"
+                onChange={(e) => {
+                  setInputSearch(e.target.value);
+                }}
+                type="text"
+              />
+              <img src={Search} alt="" />
+            </div>
+          </div>
+          <div className="individualChatContainer">
+            {constants
+              .filter((name) => name.toLowerCase().includes(inputSearch))
+              .map((name, key) => (
+                <Chat key={key} index={key} name={name} />
+              ))}
+          </div>
+        </div>
+        {openChat == null ? <WelcomePage /> : <OpenChat />}
+        {openChat == null ? null : (
+          <div className="actAsRightSection">
+            <div className="actAsRightSectionTopCard">
+              <div className="actAsRightSectionTopCardImg">
+                <img
+                  src={`https://avatars.dicebear.com/api/male/${openChat.img}.svg`}
+                  alt=""
+                />
+                Name
+              </div>
+              <div>
+                <div>
+                  <div>Created On</div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>Last Modified</div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>Total Tokens Used</div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>Recent Generated Tokens</div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div className="actAsRightSectionBottomContent">
+              <div>Chat Summary</div>
+              <div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
