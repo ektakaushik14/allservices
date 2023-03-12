40a7b1455cd88b6d91c3790497520e9cca8581f5
Ektakaushik <105712212+Ektakaushik@users.noreply.github.com>
2023-03-12 17:51:36 +0530

    pptfeatures

--- src/Components/PptDashboard/Createppt.js
+++ src/Components/PptDashboard/Createppt.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Createppt.css";
+import colorPalette from "../../Images/color-palette.png";
+import plus from "../../Images/plus.png";
+import { addPlusFeatureContent } from "../../utils/addPlusFeatureContent";
 export default function Createppt() {
+  const [isActive, setIsActive] = useState(false);
+  const onplusClick = () => {
+    let plusContent = document.querySelector(".plusContent");
+    if (plusContent.style.display === "none") {
+      plusContent.style.display = "block";
+    } else {
+      plusContent.style.display = "none";
+    }
+    // setIsActive(!isActive);
+  };
+
   return (
     <div className="Createppt">
       <div></div>
@@ -20,8 +34,96 @@ export default function Createppt() {
       </div>
       <div>
         <div>
-          <img src="https://img.icons8.com/ios-glyphs/256/plus-math.png" />
-          <img src="https://img.icons8.com/external-those-icons-lineal-those-icons/256/external-Palette-design-those-icons-lineal-those-icons.png" />
+          <img onClick={onplusClick} src={plus} />
+          <img src={colorPalette} />
+        </div>
+      </div>
+      <div
+        // style={isActive === true ? { display: "block" } : { display: "none" }}
+        className="plusContent"
+      >
+        <div>
+          <input type="text" placeholder="Add something..." />
+        </div>
+        <div>
+          {addPlusFeatureContent.map((tempAdd, index) => (
+            <div className="ptcard" key={index}>
+              <div>
+                <img src={tempAdd.img} alt="" />
+              </div>
+              <div>{tempAdd.name}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="paletteContent">
+        <div>
+          <h4>Set theme</h4>
+          <div>
+            <img src="https://img.icons8.com/material/1x/delete-sign.png" />
+          </div>
+        </div>
+        <div>
+          <button>Page</button>
+        </div>
+        <div>
+          <h5>Page theme</h5>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h3>
+                Title<span>Body</span>
+              </h3>
+            </div>
+            <div>Dark</div>
+          </div>
+          <div>
+            <img src="https://img.icons8.com/external-others-amoghdesign/1x/external-chevron-multimedia-solid-30px-others-amoghdesign.png" />
+          </div>
+        </div>
+        <div>Page</div>
+        <div>
+          <div></div>
+          <div>000000</div>
+        </div>
+        <div>
+          <div>
+            <h5>Heading</h5>
+            <div>
+              <div>Diatype</div>
+              <img src="https://img.icons8.com/external-others-amoghdesign/1x/external-chevron-multimedia-solid-30px-others-amoghdesign.png" />
+            </div>
+            <div>
+              <div>Bold</div>
+              <img src="https://img.icons8.com/external-others-amoghdesign/1x/external-chevron-multimedia-solid-30px-others-amoghdesign.png" />
+            </div>
+            <div></div>
+            <div>
+              <div></div>
+              <div>000000</div>
+            </div>
+          </div>
+          <div>
+            <h5>Paragraph</h5>
+            <div>
+              <div>Diatype</div>
+              <img src="https://img.icons8.com/external-others-amoghdesign/1x/external-chevron-multimedia-solid-30px-others-amoghdesign.png" />
+            </div>
+            <div>
+              <div>Regular</div>
+              <img src="https://img.icons8.com/external-others-amoghdesign/1x/external-chevron-multimedia-solid-30px-others-amoghdesign.png" />
+            </div>
+            <div></div>
+            <div>
+              <div></div>
+              <div>000000</div>
+            </div>
+          </div>
+        </div>
+        <div>Auto color</div>
+        <div>
+          <button>Save theme</button>
         </div>
       </div>
     </div>
---
+++ src/utils/addPlusFeatureContent.js
@@ -0,0 +1,44 @@
+const addPlusFeatureContent = [
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Sales Pitch",
+    img: "https://images.unsplash.com/photo-1623697899813-cf12fa9bc1c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRpbmclMjBwYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+export { addPlusFeatureContent };
